5cdc62da14dafbcc835d1e4dccb6a7709b6cbda8
Lilla02 <91310835+Lilla02@users.noreply.github.com>
2021-09-29 11:02:17 +0200

    Add files via upload

---
+++ 1óra.py
@@ -0,0 +1,7 @@
+number1 = int(input("Adjon meg egy számot: "))
+if number1 > 10:
+    print(number1, "nagyobb, mint 10.")
+elif number1 == 10:
+    print(number1, "egyenlő 10-el.")
+elif number1 < 10:
+    print(number1, "kisebb, mint 10.")
