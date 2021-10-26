fd37871d596532cdaeb835763391c54aa7b4530e
Lilla02 <91310835+Lilla02@users.noreply.github.com>
2021-10-26 19:13:10 +0200

    Add files via upload

---
+++ 3.óra/intervallum.py
@@ -0,0 +1,6 @@
+number = int(input("Kérek egy számot:"))
+if number >= 3 and number < 17:
+    print("Beleesik a szám a [3, 17) intervallumba.")
+else:
+    print("A szám nem esik bele a [3, 17) intervallumba.")
+
