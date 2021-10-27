c4ff1321ce8f70619479317e8c045aa32ef1ed5e
Lilla02 <91310835+Lilla02@users.noreply.github.com>
2021-10-27 10:30:11 +0200

    Add files via upload

---
+++ 6.óra/szökőév.py
@@ -0,0 +1,11 @@
+ev = int(input("Kérek egy évszamot:"))
+if ev % 4 == 0:
+    if ev % 100 == 0:
+        if ev % 400 == 0:
+            print("szökőév")
+        else:
+            print("nem szökő év")
+    else:
+        print("szökőév")
+else:
+   print("nem szökő év")
